76527ea2053d695bc8a53cbbc2d6773f8f2df525
quangdang <quangdang@lecle.co.kr>
2022-06-18 11:05:42 +0700

    feat: :sparkles: build UI header part #2

---
+++ src/components/AccountItem/AccountItem.jsx
@@ -0,0 +1,28 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/ad7ccf5f9dfffae9994dec8e8cd7f2c5~c5_300x300.webp?x-expires=1655694000&x-signature=IKc4MqRNOhf%2Fhzq%2B3%2BJTHBQvtY8%3D"
+        alt="Hoaa"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
